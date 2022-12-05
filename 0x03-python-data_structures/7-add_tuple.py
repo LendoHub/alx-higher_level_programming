3cf6dd064971aac78e109f7b8d3c341a8a29774f
LendoHub <lendodlic2022@gmail.com>
2022-12-05 23:47:11 +0000

    function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-
-def multiple_returns(sentence):
-    if sentence != '':
-        first_char = sentence[0]
-    else:
-        first_char = None
-    return (len(sentence), first_char)
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
